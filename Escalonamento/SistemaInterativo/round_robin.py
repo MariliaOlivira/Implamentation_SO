117b1327f0196e83201ad10d794af1a5a7e8df9a
Francisco Neto <bezerra.neto@academico.ifpb.edu.br>
2022-11-16 10:11:03 -0300

    refatoração do método round_robin

--- Escalonamento/SistemaInterativo/round_robin.py
+++ Escalonamento/SistemaInterativo/round_robin.py
@@ -1,43 +1,49 @@
-from process_getters import get_process_list
+from process_getters import get_process_list, new_process_bach
 import time
 
 quantum = 20
 
-def round_robin(process_dict):
+def round_robin(process_dict, to_add = False, round = 1):
   if len(process_dict) == 0:
     return
 
-  total_time = 0
+  if not to_add:
+    to_add = True
+
+  to_del = []
+  print(f"Executando a rodada {round}")
+
   for i in process_dict:
-    total_time += int(process_dict[i][1])
+    process_info = process_dict[i]
+    process_service_time = int(process_info[1])
 
+    print(f'''
+    \rExecutando {i}:
+      \r\ttempo de execução: {process_service_time}
+      \r\ttempo de quantum: {quantum}
+      \r\tcomando de invocação: {process_info[3]}
+      \r\tusuario: {process_info[-1]}
+    ''')
 
-  while(total_time >= 0):
-    to_del = []
-    for i in process_dict:
-      process_info = process_dict[i]
-      process_service_time = int(process_info[1])
+    time.sleep(quantum/10)
 
-      print(f'''
-      \r\tExecutando {i}:
-        \r\ttempo de execução: {process_service_time}
-        \r\ttempo de quantum: {quantum}
-        \r\tcomando de invocação: {process_info[3]}
-        \r\tusuario: {process_info[-1]}
-      ''')
+    process_service_time-=quantum
 
-      time.sleep(quantum/10)
+    if process_service_time <= 0:
+      to_del.append(i)
 
-      total_time-=quantum
-      process_service_time-=quantum
+    process_info[1] = process_service_time
 
-      if process_service_time <= 0:
-        to_del.append(i)
+  if to_add and round == 1:
+    print("Recebendo novos processos\n")
+    new_process_bach('casos_teste/arq_sis_interativo2.csv', process_dict)
 
-      process_info[1] = process_service_time
+  for key in to_del:
+    del process_dict[key]
 
-    for key in to_del:
-      del process_dict[key]
+  round += 1
+  round_robin(process_dict, True, round)
+  return
 
 process_dict:dict = get_process_list('casos_teste/arq_sis_interativo.csv')
 round_robin(process_dict)
